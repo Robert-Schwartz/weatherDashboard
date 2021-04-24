943253430789306eb74f1ca9325871409bd7d1a3
Robert Schwartz <bschwartz23@gmail.com>
2021-04-24 10:54:05 -0700

    psuedo structure

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+//---------------API's--------------------
+var apiKey = "831c11d2cb8f0310ab1b8fd1a37234be";
+var oneCallApi ="https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}";
+
+
+//---------------HTML Selectors--------------------
+
+
+
+//---------------Search for a city---------------
+
+
+//---------------display current and future conditions for that city---------------
+
+
+
+//---------------Add city to search history---------------
+
+
+ /*---------------display weather conditions, name, date, weather icon, temperature, humidity, wind speed, UV index---------------*/
+
+
+//---------------uv index conditional to display favorable, moderate, or severe---------------
+
+
+//---------------display 5 day forecast---------------
+
+
+//---------------click on search history to change displayed city---------------
+
